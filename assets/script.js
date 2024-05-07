6fd6fd70704900fb067b7cabd824ce9fc4e12ae1
Jan Schwegler <schwegler.jan@gmail.com>
2024-05-07 14:13:17 +0200

    create start animation & remove arrows

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,35 @@ var swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
       clickable: true,
     },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
   });
+
+let userinteraction = false;
+swiper.on('touchStart', function() {
+  userinteraction = true;
+});
+
+// swiper start animations
+setTimeout(function() {
+  if (!userinteraction) {
+    startSliderAnimationFunction();
+  }
+}, 3000);
+
+
+function startSliderAnimation() {
+  setTimeout(function() {
+    if (!userinteraction) {
+      startSliderAnimationFunction();
+    }
+  }, 6000);
+}
+
+function startSliderAnimationFunction() {
+  let swiperWrapper = document.querySelector('.swiper-wrapper');
+  swiperWrapper.style.animation = 'none';
+  setTimeout(function() {
+      swiperWrapper.style.animation = '';
+  }, 10);
+  swiperWrapper.classList.add('start-animation');
+  startSliderAnimation();
+}
